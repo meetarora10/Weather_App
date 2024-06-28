68c59ae7ae13ea13f832bbe389e670d6b58956d5
meetarora <meet.arora2403@gmail.com>
2024-06-28 11:24:45 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Container from './components/Container'
+const App = () => {
+  return (
+    <div className='weather'>
+      <Container />
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/components/Container.jsx
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react'
+import './Container.css'
+import { useState,useEffect } from 'react'
+import clear from '../images/clear.png'
+import cloud from '../images/clear.png'
+import drizzle from '../images/clear.png'
+import rain from '../images/clear.png'
+import snow from '../images/clear.png'
+import wind from '../images/wind.png'
+import humid from '../images/humidity.png'
+const Container = () => {
+  const inputCity=useRef();
+  const[pdata,setPdata]=useState(false);
+  const returnData = async(city)=>{
+    if(city===""){
+      alert("Enter city name");
+      return;
+    }
+    try{
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_API}`;
+      const res= await fetch(url);
+      const data=await res.json();
+      if(!res.ok){
+        alert("Enter valid city name");
+        return;
+      }
+      setPdata({
+        name:data.name,
+        temp:data.main.temp,
+        humidity:data.main.humidity,
+        icon:`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+        speed:data.wind.speed,
+      })
+      console.log(data);
+      console.log(data.wind.speed);
+      console.log(data.main.temp);
+      console.log(data.main.humidity);
+    }  catch(e){
+      setPdata(false);
+    }
+  }
+  useEffect(()=>{
+    returnData('Florida');
+  },[])
+
+  return (
+    <div className='container'>
+        <div className="search">
+            <input ref={inputCity} type="text" placeholder='Enter City' />
+            <img src="https://i.pinimg.com/736x/fa/0e/7b/fa0e7b992eff03c576727e95c746005c.jpg" alt="" srcset="" onClick={()=>returnData(inputCity.current.value)}/>
+        </div>
+        <img src={pdata.icon} alt="img" srcset="" className='icon'/>
+        <p className='temp'>{pdata.temp}°C</p>
+        <p className='place'>{pdata.name}</p>
+        <div className='other'>
+          <div className="wind">
+            <p className='speed'>{pdata.speed}km/h</p>
+            <p className='display'>Wind Speed</p>
+          </div>
+          <div className="humid">
+            <p className='ity'>{pdata.humidity}%</p>
+            <p className='display'>Humidity</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Container
